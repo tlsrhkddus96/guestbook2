255432164bde623dd10e997dd1e741f5653612aa
tlsrhkddus96 <tlsrhkddus96@users.noreply.github.com>
2021-07-11 15:29:17 +0900

    Initial commit

---
+++ src/main/java/org/zerock/guestbook2/Guestbook2Application.java
@@ -0,0 +1,13 @@
+package org.zerock.guestbook2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Guestbook2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Guestbook2Application.class, args);
+	}
+
+}
