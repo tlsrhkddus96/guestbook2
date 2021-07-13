2457fac08c00b0b1f259f4f10b81ec8217066936
tlsrhkddus96 <tlsrhkddus96@users.noreply.github.com>
2021-07-13 19:28:37 +0900

    Guestbook(1)

--- src/main/java/org/zerock/guestbook2/controller/GuestbookController.java
+++ src/main/java/org/zerock/guestbook2/controller/GuestbookController.java
@@ -1,21 +1,36 @@
 package org.zerock.guestbook2.controller;
 
+import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.zerock.guestbook2.dto.PageRequestDTO;
+import org.zerock.guestbook2.service.GuestbookService;
 
 @Controller
 @RequestMapping("/guestbook")
 @Log4j2
+@RequiredArgsConstructor
 public class GuestbookController {
 
-    @GetMapping({"/","/list"})
-    public String list(){
+    private final GuestbookService service;
 
-        log.info("list.....");
+    @GetMapping("/")
+    public String index(){
 
-        return "/guestbook/list";
+        return "redirect:/guestbook/list";
+    }
+
+
+    @GetMapping("/list")
+    public void list(PageRequestDTO pageRequestDTO, Model model){
+
+        log.info("list....." + pageRequestDTO);
+
+        model.addAttribute("result", service.getList(pageRequestDTO));
+
     }
 
 }
